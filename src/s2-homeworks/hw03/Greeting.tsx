9a3685e8a2b27b1fa01e9bebea28cd90a1ee952f
Siarhei <Mohovtsovserg123@gmail.com>
2025-11-21 00:21:01 +0300

    hw 3 is done

--- src/s2-homeworks/hw03/Greeting.tsx
+++ src/s2-homeworks/hw03/Greeting.tsx
@@ -29,45 +29,46 @@ const Greeting: React.FC<GreetingPropsType> = (
     const inputClass = s.errorInput ? "Ошибка! Введите имя!" : '' // need to fix with (?:)
 
     return (
-        <div id={'hw3-form'} className={s.greetingForm}>
-            <div className={s.text}>
-                {'Людей добавили: '}
-                <span id={'hw3-users-total'}>
-                    {totalUsers}
-                </span>
+<div id={'hw3-form'} className={s.greetingForm}>
+    <div className={s.text}>
+        {'Людей добавили: '}
+        <span id={'hw3-users-total'}>
+            {totalUsers}
+        </span>
+    </div>
+
+    <div className={s.inputAndButtonContainer}>
+        <div>
+            <input
+                id={'hw3-input'}
+                value={name}
+                onChange={setNameCallback}
+                className={`${s.input} ${error ? s.errorInput : ''}`}
+                onKeyDown={onEnter}
+                onBlur={onBlur}
+            />
+            <div id={'hw3-error'} className={s.error}>
+                {error}
             </div>
-
-            <div className={s.inputAndButtonContainer}>
-                <div>
-                    <input
-                        id={'hw3-input'}
-                        value={name}
-                        onChange={setNameCallback}
-                        className={inputClass}
-                        onKeyDown={onEnter}
-                        onBlur={onBlur}
-                    />
-                    <div id={'hw3-error'} className={s.error}>
-                        {error}
-                    </div>
-                </div>
-
-                <button
-                    id={'hw3-button'}
-                    onClick={addUser}
-                    className={s.button}
-                    disabled={!name.trim()}
-                >
-                    add
-                </button>
-            </div>
-
-            {lastUserName && (
-                <div className={s.greeting}>
-                    Привет <span id={'hw3-last-user'}>{lastUserName}</span>!
-                </div>
-            )}
         </div>
+
+        <button
+            id={'hw3-button'}
+            onClick={addUser}
+            className={s.button}
+            disabled={!name.trim()}
+        >
+            add
+        </button>
+    </div>
+
+    {lastUserName && (
+        <div className={s.greeting}>
+            Привет <span id={'hw3-last-user'}>{lastUserName}</span>!
+        </div>
+    )}
+</div>
+
     )
 }
 
